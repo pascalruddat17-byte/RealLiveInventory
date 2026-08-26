2503f198d144179a9b48eb85748fff13ef5972b5
pascalruddat17-byte <pascal.ruddat17@gmail.com>
2026-08-26 20:45:33 +0200

    Update inventory app

--- docs/sw.js
+++ docs/sw.js
@@ -1,9 +1,10 @@
-const CACHE_NAME = "reallife-inventory-v2";
+const CACHE_NAME = "reallife-inventory-v11";
 const APP_SHELL = [
   "./",
   "./index.html",
   "./manifest.webmanifest",
-  "./icon.svg"
+  "./icon.svg",
+  "./profile-default.png"
 ];
 
 self.addEventListener("install", (event) => {

255ad8dd23edc27de46d13b9c8eb084f4b19b941
Emolio <pascal.ruddat17@gmail.com>
2026-08-26 20:53:45 +0200

    Apply backpack visual style

--- docs/sw.js
+++ docs/sw.js
@@ -1,9 +1,11 @@
-const CACHE_NAME = "reallife-inventory-v11";
+const CACHE_NAME = "reallife-inventory-v12";
 const APP_SHELL = [
   "./",
   "./index.html",
   "./manifest.webmanifest",
   "./icon.svg",
+  "./app-icon.png",
+  "./backpack-icon.png",
   "./profile-default.png"
 ];
 
